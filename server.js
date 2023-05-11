b543cd7790efad3d951c84d2753155f213510dce
imani.devs <imaniroberts.dev@gmail.com>
2023-05-10 21:53:20 -0400

    connected database, can see json in browser

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const pokemon = require("./models/pokemon.js");
+
+app.set("view engine", "jsx"); //sets the view engine to jsx
+app.engine("jsx", require("jsx-view-engine").createEngine()); //sets the view engine to jsx
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Pokemon App!");
+});
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
+});
+
+app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
+});
